33ff45bb92f4fa15a6c6954d6f799e75860f0134
amiradmehr <amir.radmehr77@gmail.com>
2021-09-19 11:10:56 +0430

    split subject for emails

--- gsheet.py
+++ gsheet.py
@@ -44,7 +44,7 @@ class Gsheet:
 
         df = deepcopy(self.res)
 
-        index_names = df[df[ 'Subject'] == '' ].index
+        index_names = df[df[ 'Subject I'] == '' ].index
         df.drop(index_names, inplace = True)
 
         rows = df.loc[df['Log']!='sent']
